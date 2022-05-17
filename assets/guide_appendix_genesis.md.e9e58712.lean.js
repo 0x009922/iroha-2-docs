90e9d8f7596e0f44900e0f0ebf19b3abf9196cc3
appetrosyan <appetrosyan@https://users.noreply.github.com/>
2022-05-17 16:22:40 +0000

    Auto update pages on 2022-05-17 16:22:40

---
+++ assets/guide_appendix_genesis.md.e9e58712.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,d as l}from"./app.0f261f37.js";const d='{"title":"Genesis Block","description":"","frontmatter":{},"headers":[],"relativePath":"guide/appendix/genesis.md","lastUpdated":1652804524000}',o={},p=l("",3),e=[p];function t(c,r,y,i,u,F){return a(),n("div",null,e)}var A=s(o,[["render",t]]);export{d as __pageData,A as default};
